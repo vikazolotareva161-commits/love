4298eba20d704482eba2d86a7121742409bc5a2e
Диана Волк <dianawolf@MacBook-Pro-Diana.local>
2026-07-12 13:25:37 +0300

    add github pages base

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { viteSingleFile } from "vite-plugin-singlefile";
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: "/love/",
     plugins: [react(), tailwindcss(), viteSingleFile()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
